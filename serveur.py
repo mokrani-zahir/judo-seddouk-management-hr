6463bed41170c45bf5673fe379267ae7690bea28
mokrani-zahir <zahir1356@gmail.com>
2026-09-10 19:04:45 +0200

    v1.1.7 - test final de la mise a jour complete (local 1.1.6 -> 1.1.7)

--- serveur.py
+++ serveur.py
@@ -27,7 +27,7 @@ HOST = "127.0.0.1"
 REPO = "mokrani-zahir/judo-seddouk-management-hr"
 RAW_PREFIX = "https://raw.githubusercontent.com/%s/master" % REPO
 VERSION_JSON_URL = RAW_PREFIX + "/version.json"
-APP_VERSION = "1.1.6"
+APP_VERSION = "1.1.7"
 
 
 def update_json_url():
